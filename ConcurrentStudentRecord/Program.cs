721ed3ba6665ff041066673de6a7f7d3b4ce14e8
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2021-01-08 05:34:04 -0800

    Works Fine

--- ConcurrentStudentRecord/Program.cs
+++ ConcurrentStudentRecord/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,58 @@ namespace ConcurrentStudentRecord
 {
     class Program
     {
+        // To teacher trying to input the student record in a
+        // same dictionary.
+
+        static ConcurrentDictionary<int,Student> cd = new ConcurrentDictionary<int,Student>();
         static void Main(string[] args)
         {
+
+            List<Task> tk = new List<Task>();
+
+
+            tk.Add(Task.Factory.StartNew(() => Teacher1()));
+
+           tk.Add( Task.Factory.StartNew(() => Teacher2()));
+
+            Task.WaitAll(tk.ToArray());
+
+           
+
+            foreach (var x in cd)
+            {
+                Console.WriteLine(x.Key +" " + x.Value.name);
+            }
+
+
+
         }
+
+
+        public static void Teacher1()
+        {
+            foreach(var x in Student.GetStudentRecord() )
+            {
+                 cd.TryAdd(x.id, x);
+                
+            }
+
+
+        }
+
+        public static void Teacher2()
+        {
+            foreach (var x in Student.GetStudentRecord())
+            {
+                   cd.TryAdd(x.id, x);
+               
+            }
+
+        }
+
+
+
+
+
     }
 }
---
+++ ConcurrentStudentRecord/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentStudentRecord
+{
+    class Student
+    {
+        public int id;
+        public string name;
+        public int marks;
+
+        public Student(string n,int m, int i)
+        {
+            name = n;
+            marks = m;
+            id = i;
+        }
+
+
+
+        public static List<Student> GetStudentRecord()
+        {
+            List<Student> l = new List<Student>()
+            {
+                new Student("kjckaj",45,1),
+                 new Student("aaaa",50,2),
+                  new Student("bbbb",74,3),
+                   new Student("cccc",23,4),
+                    new Student("ddddd",78,5)
+
+
+
+            };
+
+            return l;
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
